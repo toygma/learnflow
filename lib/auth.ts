073a094e04e77855fbc7de1a0e94c97a42777d2a
toygma <toygunbektasoglu@gmail.com>
2025-09-09 21:47:05 +0300

    feat:t3-zod

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { env } from "./env";
 
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders:{
+    github:{
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+    }
+  }
 });
