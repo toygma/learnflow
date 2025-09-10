9cd24eb0a38fcb9be5099e744f140673bc133d85
toygma <toygunbektasoglu@gmail.com>
2025-09-10 22:14:05 +0300

    feat: arcjet file structure established

--- lib/env.ts
+++ lib/env.ts
@@ -8,7 +8,8 @@ export const env = createEnv({
     BETTER_AUTH_URL: z.string().url() ,
     AUTH_GITHUB_CLIENT_ID: z.string().min(1),
     AUTH_GITHUB_CLIENT_SECRET: z.string().min(1),
-    RESEND_API_KEY: z.string().min(1)
+    RESEND_API_KEY: z.string().min(1),
+    ARCJET_KEY: z.string().min(1)
   },
  experimental__runtimeEnv: {
   }
