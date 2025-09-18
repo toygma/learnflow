c8d3124fa0bedae117ee85cb79a52113bb94b516
toygma <toygunbektasoglu@gmail.com>
2025-09-18 21:53:06 +0300

    feat: zod schema

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const CoursesCreate = () => {
   return (
-    <div>CoursesCreate</div>
-  )
-}
+    <>
+      <div className="mb-8 flex items-center gap-4">
+        <Link
+          href={"/admin/courses"}
+          className={buttonVariants({ variant: "ghost", size: "icon" })}
+        >
+          <ArrowLeftIcon className="size-4" />
+        </Link>
+        <h1 className="text-2xl font-bold">Create Course</h1>
+      </div>
 
-export default CoursesCreate
+      <Card>
+        <CardHeader>
+          <h2 className="text-lg font-bold">Basic information</h2>
+          <CardDescription></CardDescription>
+        </CardHeader>
+      </Card>
+    </>
+  );
+};
+
+export default CoursesCreate;
---
+++ lib/zodSchema.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+export const courseStatus = ["Draft", "Published", "Archived"] as const;
+
+export const courseSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be at least 3 characters long" })
+    .max(100, { message: "Title must be at most 100 characters long" }),
+
+  description: z
+    .string()
+    .min(10, { message: "Description must be at least 10 characters long" })
+    .max(500, { message: "Description must be at most 500 characters long" }),
+
+  smallDescription: z
+    .string()
+    .min(3, { message: "Short description must be at least 3 characters long" })
+    .max(200, {
+      message: "Short description must be at most 200 characters long",
+    }),
+
+  fileKey: z.string().min(1, { message: "File key is required" }),
+
+  price: z.coerce.number().min(1, { message: "Price must be at least 1" }),
+
+  duration: z.coerce
+    .number()
+    .min(1, { message: "Duration must be at least 1 hour" })
+    .max(500, { message: "Duration cannot exceed 500 hours" }),
+
+  level: z.enum(courseLevels, { message: "Level is required" }),
+
+  category: z.string().min(1, { message: "Category is required" }),
+
+  slug: z
+    .string()
+    .min(3, { message: "Slug must be at least 3 characters long" })
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+      message: "Slug must be lowercase, alphanumeric, and can contain hyphens",
+    }),
+
+  status: z.enum(courseStatus).optional(),
+});
+export type CourseSchema = z.infer<typeof courseSchema>;
